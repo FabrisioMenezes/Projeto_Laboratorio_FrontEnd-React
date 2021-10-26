a07c573d53ccf5c68f92019600365f92fc824c62
scaduda <samira.caduda@souunit.com.br>
2021-10-25 23:23:54 -0300

    add: material ui e redux

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import { createTheme, ThemeProvider } from '@material-ui/core/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#5E81AC',
+    },
+    secondary: {
+      main: '#EEEEEE',
+    },
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <ThemeProvider theme={theme}>
+        <div className="App">
+          <Button variant="contained" color="primary">
+            Hello World
+          </Button>
+        </div>
+      </ThemeProvider>
   );
 }
 
--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,12 @@
-import React from 'react'
-import {Switch, Route, Redirect} from 'react-router-dom
-import Login from './pages/Login'
+import React, { Suspense, Lazy} from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-const Routes = () =>{
-    return  (
-        <Switch>
-            <Route component={Login} exact path="/" />
-            <Route path="*" >
-                <Redirect to="/" />
-            </Route>
-        </Switch>
-    )
-}
-
-export default Routes
+const Routes = () => (
+    <Router>
+        <Suspense fallback={<div></div>}>
+            <Switch>
+                <Route path="/login" component={}/>
+            </Switch>
+        </Suspense>
+    </Router>
+)
